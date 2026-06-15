d753576b6f22ccf744af63431de7976d60becbfa
matchship <lylekylestyle@gmail.com>
2026-06-15 11:12:25 +0545

    Edited forms.py, urls.py and views.py

--- DjangoProjects/Register_Login/django_project/core/urls.py
+++ DjangoProjects/Register_Login/django_project/core/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 
 urlpatterns = [
-
+    path('', views.landing_page, name='landing'),
+    path('register/', views.register_view, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('home/', views.home_view, name='home'),
+    path('logout/', views.logout_view, name='logout'),
 ]
--- DjangoProjects/Register_Login/django_project/core/views.py
+++ DjangoProjects/Register_Login/django_project/core/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import UserRegistrationForm
 
 # Create your views here.
+
+def landing_page(request):
+    return render(request, 'core/landing.html')
+
+def register_view(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            messages.success(request, 'Registration successful.')
+            return redirect('login')
+        else:
+            messages.error(request, 'Please correct following errors')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'core/register.html', {'form':form})
+
+def login_view(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        user = authenticate(request, email=email, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Inavlid email or password')
+        
+    return render(request, 'core/login.html')
+
+
+@login_required
+def home_view(request):
+    return render(request, 'core/home.html')
+
+
+def logout_view(request):
+    logout(request)
+    messages.info(request, 'YOu have been successfully logged out.')
+    return redirect('landing')
+
+
+
+
+
